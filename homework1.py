d00d4ead798827173a167d1a61376047ba1f491f
itsmariianna <138693244+itsmariianna@users.noreply.github.com>
2024-05-30 12:57:15 +0400

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,12 @@
+#1. Print Hello, world!
+print("Hello, world!")
+
+#2. Print your name and surname
+print("Marianna Bakhshetsyan")
+
+#3. Check if number is even or odd
+number = int(input("Enter number: "))
+if number % 2 == 0:
+    print("Entered number is even")
+else:
+    print("Entered number is odd")
