966baaa56271b7eae616bbe6936445c04424ee29
itsmariianna <138693244+itsmariianna@users.noreply.github.com>
2024-05-31 17:21:13 +0400

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+# #1. Write a program that asks the user for weight in kilograms and converts it to pounds. There are 2.2 pounds in a kilogram.
+# kg = int(input("Enter weight in kilograms: "))
+# pound = kg * 2.2
+# print(f'{kg} kilograms = {pound} pounds')
+
+
+# #2. Write a program that asks the user to enter two numbers, x, and y, and computes x − y  /  x+y.
+# x = int(input("Enter first number: "))
+# y = int(input("Enter second number: "))
+# result = x - y / x + y
+# print("The result is: ", result)
