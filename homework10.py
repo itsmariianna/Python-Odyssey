e60a2cbe766e675e191b5f9ae8da7fb705832b23
itsmariianna <138693244+itsmariianna@users.noreply.github.com>
2024-07-04 15:42:37 +0400

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,24 @@
+# # Ունենք ամբողջ թվերից բաղկացած մատրից, որի վրա պետք է իրականացնել transpose։ Transpose կատարել նշանակում է մատրիցի տողերը փոխադրել սյուներով:
+# matrix = [[1, 2, 3]       
+#          ,[4, 5, 6]
+#          ,[7, 8, 9]]
+
+# size_matrix = len(matrix)
+
+# for i in range(size_matrix):
+#     for j in range(i + 1, size_matrix):
+#         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+# print(matrix)
+
+
+
+# # Գրել ծրագիր, որը քառակուսային մատրիցը շրջում է 180 աստիճան։
+# matrix = [[1, 2, 3, 4],
+#           [5, 6, 7, 8],
+#           [9, 10, 11, 12],
+#           [13, 14, 15, 16]]
+
+# for i in matrix:
+#     i.reverse()
+# matrix.reverse()
+# print(matrix)
